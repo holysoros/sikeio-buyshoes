d9553347d96ec48c44cdbf021ce2553b79778d9e
LiJunjie <holysoros@gmail.com>
2016-01-20 10:21:55 +0800

    Exercise: Use Perfect Scrolling

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+function makeCartScrollNicely() {
+  var cart = document.querySelector(".site__right-sidebar .cart");
+  Ps.initialize(cart);
+}
+
 window.onload = function() {
   console.log("page loaded");
+  makeCartScrollNicely();
 }
